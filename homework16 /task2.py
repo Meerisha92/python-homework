96335e8d15dc46b6e3a92a91f9dc742f5f9c6958
Meerisha92 <meerimulanbek@ymail.com>
2025-06-20 01:37:18 +0000

    homework16

---
+++ homework16 /task2.py
@@ -0,0 +1,8 @@
+number=float(input("enter a number: "))
+if number > 0:
+    print("he nuber is positive")
+elif number < 0:
+    print("the nuber is negative")
+else:
+    print("the number is zero")
+    
---
+++ homework16 /task3.py
@@ -0,0 +1,6 @@
+name=input("enter your name")
+
+if name == "Alice" or name == "Bob": 
+    print(f"hello, {Bob}! Welcome to our store!")
+else: 
+    print("hello, guest!")
